d73778e2f55b556f6e9bef294cdb6fe963648846
Julia Kozlova <kozjuliajulia@yandex.ru>
2025-06-08 22:33:43 +0300

    fix templates

--- src/main/java/ru/yandex/practicum/intershop/configuration/constants/TemplateConstants.java
+++ src/main/java/ru/yandex/practicum/intershop/configuration/constants/TemplateConstants.java
@@ -5,12 +5,12 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class TemplateConstants {
 
-    public static final String TEMPLATE_CART = "/cart";
-    public static final String TEMPLATE_MAIN = "/main";
-    public static final String TEMPLATE_ITEM = "/item";
-    public static final String TEMPLATE_ORDER = "/order";
-    public static final String TEMPLATE_ORDERS = "/orders";
-    public static final String TEMPLATE_ADD_ITEM = "/add-item";
+    public static final String TEMPLATE_CART = "cart";
+    public static final String TEMPLATE_MAIN = "main";
+    public static final String TEMPLATE_ITEM = "item";
+    public static final String TEMPLATE_ORDER = "order";
+    public static final String TEMPLATE_ORDERS = "orders";
+    public static final String TEMPLATE_ADD_ITEM = "add-item";
     public static final String REDIRECT_ITEMS = "redirect:/items";
     public static final String REDIRECT_ORDERS = "redirect:/orders";
     public static final String REDIRECT_CART_ITEMS = "redirect:/cart/items";
